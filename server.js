ecd06fe912b190cefe4ae0815009e258a42e4a47
GM <mrgavintech@gmail.com>
2019-08-25 13:58:13 -0500

    add helmet for added security

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const expressBasicAuth = require('express-basic-auth');
 const reqErrorHandler = require('./helper/request-error-handler.js');
 const routes = require('./routes/routes.js');
 const app = express();
-const config = require('config');
+const helmet = require('helmet');
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Worker started on PORT ' + server.address().port + ' and PID is ' + process.pid);
 });
 
 app.use(
+    helmet(),
     bodyParser.json(),
     expressBasicAuth({
         users: { AppAdmin: 'password' }
